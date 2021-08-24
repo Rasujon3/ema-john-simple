260960a4f8948f02b91a90371f09701846482f7d
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-08-24 12:52:58 +0600

    End of 11
    Product add to card dynamically

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -5,10 +5,13 @@ import "./Shop.css";
 
 const Shop = () => {
   const first10 = fakeData.slice(0, 10);
-  const [products, setProducts] = useState(first10);
+  const [products] = useState(first10);
+  const [cart, setCart] = useState([]);
 
   const handleAddProduct = (product) => {
     console.log("Product added", product);
+    const newCart = [...cart, product];
+    setCart(newCart);
   };
 
   return (
@@ -21,6 +24,7 @@ const Shop = () => {
 
       <div className="card-container">
         <h3>This is card</h3>
+        <h5>Order Summary : {cart.length}</h5>
       </div>
     </div>
   );
