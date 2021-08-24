bd2fcc4e0430fff7c5eb0569d28d3f68071088a9
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-08-24 12:40:30 +0600

    End of video 10
    Click handler add in add to cart button

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,7 +4,7 @@ import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import "./Product.css";
 
 const Product = (props) => {
-  console.log(props.product.name);
+  // console.log(props);
   const { img, name, seller, price, stock } = props.product;
   return (
     <div className="product">
@@ -23,7 +23,10 @@ const Product = (props) => {
         <p>
           <small>Only {stock} left in stock - Order soon</small>
         </p>
-        <button className="main-button">
+        <button
+          className="main-button"
+          onClick={() => props.handleAddProduct(props.product)}
+        >
           <FontAwesomeIcon icon={faShoppingCart} />
           add to cart
         </button>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,30 +1,29 @@
-import React, { useState } from 'react';
-import fakeData from '../../fakeData';
-import Product from '../Product/Product';
-import './Shop.css';
+import React, { useState } from "react";
+import fakeData from "../../fakeData";
+import Product from "../Product/Product";
+import "./Shop.css";
 
 const Shop = () => {
-    const first10 = fakeData.slice(0,10);
-    const [products,setProducts] = useState(first10);
-    
-    return (
-        <div className="shpo-container">
-            <div className="product-container">
-           
-                {
-                    products.map(pd => 
-                    <Product product={pd}></Product>)
-                }
-            
-        </div>
+  const first10 = fakeData.slice(0, 10);
+  const [products, setProducts] = useState(first10);
 
-        <div className="card-container">
-            <h3>This is card</h3>
-        </div>
-            
-            
-        </div>
-    );
+  const handleAddProduct = (product) => {
+    console.log("Product added", product);
+  };
+
+  return (
+    <div className="shpo-container">
+      <div className="product-container">
+        {products.map((pd) => (
+          <Product handleAddProduct={handleAddProduct} product={pd}></Product>
+        ))}
+      </div>
+
+      <div className="card-container">
+        <h3>This is card</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
