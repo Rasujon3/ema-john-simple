357d4d0351c8da1a276868d236a6a520621e6c7a
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-08-28 23:50:59 +0600

    cart item review page matching with fake database
    video : 7

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import fakeData from '../../fakeData';
+import { getDatabaseCart } from '../../utilities/databaseManager';
 
 const Review = () => {
+
+   const [cart, setCart] = useState([]);
+
+    useEffect(()=>{
+        // cart
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+
+        const cartProducts = productKeys.map(key => {
+            const product = fakeData.find(pd =>pd.key === key);
+            product.quantity = savedCart[key];
+            return product;
+        });
+        setCart(cartProducts);
+    },[])
+
     return (
         <div>
-            <h1>This is review</h1>
+            <h1>Cart Items : {cart.length}</h1>
         </div>
     );
 };
