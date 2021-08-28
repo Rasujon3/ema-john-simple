1fc66ad808c5f33b60a8bf7591508ac81f41ebeb
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-08-29 02:32:58 +0600

    Order Summary show in both shop & review page

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import fakeData from "../../fakeData";
-import { addToDatabaseCart } from "../../utilities/databaseManager";
+import { addToDatabaseCart, getDatabaseCart } from "../../utilities/databaseManager";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
@@ -9,6 +9,17 @@ const Shop = () => {
   const first10 = fakeData.slice(0, 10);
   const [products] = useState(first10);
   const [cart, setCart] = useState([]);
+
+  useEffect(()=>{
+    const savedCart = getDatabaseCart();
+    const productKeys = Object.keys(savedCart);
+    const previousCart = productKeys.map(existingKey => {
+      const product = fakeData.find(pd => pd.key === existingKey);
+      product.quantity = savedCart[existingKey];
+      return product;
+    })
+    setCart(previousCart);
+  },[])
 
   const handleAddProduct = (product) => {
     const toBeAddedKey = product.key;
